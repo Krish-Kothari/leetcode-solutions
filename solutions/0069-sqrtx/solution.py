a73fd9afab01180e98a2979fea083ec3b626f26e
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-02-04 10:39:26 +0000

    Sync LeetCode submission Runtime - 3 ms (63.07%), Memory - 12.3 MB (56.61%)

--- solutions/0069-sqrtx/solution.py
+++ solutions/0069-sqrtx/solution.py
@@ -4,5 +4,14 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        return(int(x**0.5))
-        
+        lo,hi=0,x//2+1
+        while lo<=hi:
+            m=(lo+hi)//2
+            sqrt=m*m
+            if sqrt==x:
+                return m
+            elif sqrt>x:
+                hi=m-1
+            elif sqrt<x:
+                lo=m+1
+        return hi
