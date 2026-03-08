d8c98be586ee7d685a148db654207a6d60274f36
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-03-08 06:15:31 +0000

    Sync LeetCode submission Runtime - 580 ms (37.59%), Memory - 12.3 MB (55.76%)

---
+++ solutions/1013-fibonacci-number/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def fib(self, n):
+        def f(n):
+            if n==0:
+                return 0
+            if n==1:
+                return 1
+            return f(n-1)+f(n-2)
+        return f(n)
