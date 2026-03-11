f2ec23b20aa143acb0affe6104d307224ec283b4
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-03-11 02:05:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.6 MB (54.20%)

---
+++ solutions/0412-fizz-buzz/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        x=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                i="FizzBuzz"
+            elif i%3==0:
+                i="Fizz"
+            elif i%5==0:
+                i="Buzz"
+            x.append(str(i))
+        return x
