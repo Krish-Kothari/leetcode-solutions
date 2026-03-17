1fd326dd7ced20226035650c9b54e596d5919c1c
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-03-17 05:23:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (5.37%)

--- solutions/0020-valid-parentheses/solution.py
+++ solutions/0020-valid-parentheses/solution.py
@@ -1,23 +1,18 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        i=0
-        a=[]
-        for i in range(len(s)):
-            if s[i]=='('or s[i]=='['or s[i]=='{':
-                a.append(s[i])
+class Solution:
+    def isValid(self, s: str) -> bool:
+        a1,a2,a3,a4,a5,a6='(',')','{','}','[',']'
+        stack=[]
+        for i in s:
+            if i==a1 or i==a3 or i==a5:
+                stack.append(i)
             else:
-                if not a:
+                if not stack:
                     return False
-                top=a.pop()
-                if s[i]==')'and top!='(':
+                top=stack.pop()
+                if i==a2 and top!=a1:
                     return False
-                if s[i]==']'and top!='[':
+                if i==a4 and top!=a3:
                     return False
-                if s[i]=='}'and top!='{':
+                if i==a6 and top!=a5:
                     return False
-        return len(a)==0
-        
+        return len(stack)==0
