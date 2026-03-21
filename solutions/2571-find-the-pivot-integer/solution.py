698be9e04997b642a470f50c1c6c9c8183872f79
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-03-21 05:00:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (23.46%)

---
+++ solutions/2571-find-the-pivot-integer/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        total=n*(n+1)//2
+        x=int(math.sqrt(total))
+        if x*x==total:
+            return x
+        return -1
