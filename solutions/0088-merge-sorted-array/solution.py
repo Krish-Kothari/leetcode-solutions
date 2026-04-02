c139a6ab5b9b69b734a1b2531e575cb48c706fd1
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-04-02 04:43:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (78.01%)

--- solutions/0088-merge-sorted-array/solution.py
+++ solutions/0088-merge-sorted-array/solution.py
@@ -1,21 +1,23 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        p1 = m - 1
-        p2 = n - 1
-        p = m + n - 1
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
+        nums1Idx=0
+        nums2Idx=0
+        result=[]
+        while nums1Idx<m and nums2Idx<n:
+            if nums1[nums1Idx]<nums2[nums2Idx]:
+                result.append(nums1[nums1Idx])
+                nums1Idx+=1
             else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            p -= 1
-        nums1[:p2 + 1] = nums2[:p2 + 1]
+                result.append(nums2[nums2Idx])
+                nums2Idx+=1
+        while nums1Idx<m:
+            result.append(nums1[nums1Idx])
+            nums1Idx+=1
+        while nums2Idx<n:
+            result.append(nums2[nums2Idx])
+            nums2Idx+=1
+        for i in range(len(result)):
+            nums1[i]=result[i]
