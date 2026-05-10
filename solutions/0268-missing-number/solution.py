864c76e7b8e6792a4c1f2285ae2e3a5d712da672
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-05-10 04:15:57 +0000

    Sync LeetCode submission Runtime - 1383 ms (13.92%), Memory - 20.2 MB (95.63%)

--- solutions/0268-missing-number/solution.py
+++ solutions/0268-missing-number/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        total_sum = n * (n + 1) // 2
-        actual_sum = sum(nums)
-        return total_sum - actual_sum
+        for i in range(0,len(nums)+1):
+            if i not in nums:
+                return i
