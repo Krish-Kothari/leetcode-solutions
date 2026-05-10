f845b4f281210e79ea69161de30855ac1abb501c
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-05-10 05:34:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (78.01%)

--- solutions/0088-merge-sorted-array/solution.py
+++ solutions/0088-merge-sorted-array/solution.py
@@ -3,21 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1Idx=0
-        nums2Idx=0
-        result=[]
-        while nums1Idx<m and nums2Idx<n:
-            if nums1[nums1Idx]<nums2[nums2Idx]:
-                result.append(nums1[nums1Idx])
-                nums1Idx+=1
-            else:
-                result.append(nums2[nums2Idx])
-                nums2Idx+=1
-        while nums1Idx<m:
-            result.append(nums1[nums1Idx])
-            nums1Idx+=1
-        while nums2Idx<n:
-            result.append(nums2[nums2Idx])
-            nums2Idx+=1
-        for i in range(len(result)):
-            nums1[i]=result[i]
+        # nums1Idx=0
+        # nums2Idx=0
+        # result=[]
+        # while nums1Idx<m and nums2Idx<n:
+        #     if nums1[nums1Idx]<nums2[nums2Idx]:
+        #         result.append(nums1[nums1Idx])
+        #         nums1Idx+=1
+        #     else:
+        #         result.append(nums2[nums2Idx])
+        #         nums2Idx+=1
+        # while nums1Idx<m:
+        #     result.append(nums1[nums1Idx])
+        #     nums1Idx+=1
+        # while nums2Idx<n:
+        #     result.append(nums2[nums2Idx])
+        #     nums2Idx+=1
+        # for i in range(len(result)):
+        #     nums1[i]=result[i]
+
+
+
+
+
+        # another submission with very simple solution
+
+        nums1[m:]=nums2
+        nums1.sort()
