47e4e026ce21c6c3c0e400758e515a7a1c42ae4e
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-05-11 13:32:56 +0000

    Sync LeetCode submission Runtime - 3 ms (70.79%), Memory - 19.7 MB (33.60%)

--- solutions/1496-lucky-numbers-in-a-matrix/solution.py
+++ solutions/1496-lucky-numbers-in-a-matrix/solution.py
@@ -1,9 +1,15 @@
 class Solution:
     def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
+        n=len(matrix)
+        m=len(matrix[0])
         x1=set()
         x2=set()
         for i in matrix:
             x1.add(min(i))
-        for i in zip(*matrix):
-            x2.add(max(i))
+        for j in range(m):
+            g=matrix[0][j] 
+            for i in range(1,n):
+                if matrix[i][j]>g:
+                    g=matrix[i][j]
+            x2.add(g)
         return list(x1&x2)
