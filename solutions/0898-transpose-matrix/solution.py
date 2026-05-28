a733d1f4d0529af2bbd86ead6aa47c94f8ce23c3
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-05-28 07:03:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20 MB (15.64%)

---
+++ solutions/0898-transpose-matrix/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        row = len(matrix)
+        col = len(matrix[0])
+        trans = [[0] * row for _ in range(col)]
+        for i in range(col):
+            for j in range(row):
+                trans[i][j] = matrix[j][i]
+        return trans
