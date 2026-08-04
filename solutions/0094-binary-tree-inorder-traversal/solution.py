1d4fb32d7147d5623c591e7baad206a6d91edc34
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-08-04 13:45:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (92.38%)

--- solutions/0094-binary-tree-inorder-traversal/solution.py
+++ solutions/0094-binary-tree-inorder-traversal/solution.py
@@ -1,11 +1,15 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
         ans=[]
         def f(x):
             if not x:
