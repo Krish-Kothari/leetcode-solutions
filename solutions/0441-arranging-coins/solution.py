d3857a4c08ace5a811d903295b5d1eb051653348
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-08-05 07:42:52 +0000

    Sync LeetCode submission Runtime - 415 ms (32.11%), Memory - 12.3 MB (88.86%)

--- solutions/0441-arranging-coins/solution.py
+++ solutions/0441-arranging-coins/solution.py
@@ -1,5 +1,9 @@
-class Solution:
-    def arrangeCoins(self, n: int) -> int:
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         i=0
         while n>=0:
             i+=1
