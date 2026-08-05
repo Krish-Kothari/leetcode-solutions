455f929c0c5cbdd963be0e7b655bdb117c1ae920
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-08-05 07:26:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (38.23%)

--- solutions/0792-binary-search/solution.py
+++ solutions/0792-binary-search/solution.py
@@ -1,15 +1,19 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n=len(nums)
         lo=0
-        hi=len(nums)-1
-        result=-1
+        hi=n-1
         while lo<=hi:
             m=(lo+hi)//2
             if nums[m]==target:
-                result=m
-                break
-            elif nums[m]<target:
+                return m
+            elif target>nums[m]:
                 lo=m+1
             else:
                 hi=m-1
-        return result
+        return -1
