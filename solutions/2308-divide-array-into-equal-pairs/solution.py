75fd3f19e4c6b8a7b697c9a167d57b3c5f708a6f
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-08-09 04:51:38 +0000

    Sync LeetCode submission Runtime - 5 ms (70.41%), Memory - 12.4 MB (97.96%)

---
+++ solutions/2308-divide-array-into-equal-pairs/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def divideArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        return all(nums[i] == nums[i + 1] for i in range(0, len(nums), 2))
+
