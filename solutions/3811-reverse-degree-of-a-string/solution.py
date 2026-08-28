12670f84327d5dc7b237df4013dafe1d48d11788
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-08-28 05:00:48 +0000

    Sync LeetCode submission Runtime - 7 ms (91.14%), Memory - 12.3 MB (89.67%)

---
+++ solutions/3811-reverse-degree-of-a-string/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseDegree(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        total=0
+        for i,ch in enumerate(s,1):
+            diff=123-ord(ch)
+            total+=diff*i
+        return total
