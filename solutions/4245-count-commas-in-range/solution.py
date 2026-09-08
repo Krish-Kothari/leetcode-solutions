aaf0f958937a458283aae11e6561c60c5d850317
Krish Kothari <79302145+Krish-Kothari@users.noreply.github.com>
2026-09-08 11:15:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (91.17%)

---
+++ solutions/4245-count-commas-in-range/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countCommas(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<1000:
+            return 0
+        if n>999 and n<10000:
+            return n-1000+1
+        if n>9999 and n<=100000:
+            return n-1000+1
